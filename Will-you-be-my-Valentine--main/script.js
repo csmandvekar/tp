f0318bdc4a127c019b612451b621f6e74c74d749
csmandvekar <csmandvekar@gmail.com>
2025-02-06 22:55:03 +0530

    commit

--- Will-you-be-my-Valentine--main/script.js
+++ Will-you-be-my-Valentine--main/script.js
@@ -8,7 +8,8 @@ const messages = [
     "I will be very sad...",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Just kidding, say yes please! ❤️",
+    "Ufff ... just saying Yes on your behalf 😘"
 ];
 
 let messageIndex = 0;
@@ -16,8 +17,17 @@ let messageIndex = 0;
 function handleNoClick() {
     const noButton = document.querySelector('.no-button');
     const yesButton = document.querySelector('.yes-button');
+
+    if (messageIndex === messages.length) {
+        // Redirect to the yes page when the last message is displayed
+        window.location.href = "yes_page.html";
+        return; // Exit function to prevent further execution
+    }
+
     noButton.textContent = messages[messageIndex];
-    messageIndex = (messageIndex + 1) % messages.length;
+    messageIndex = (messageIndex + 1);
+
+    // Increase Yes button size
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
     yesButton.style.fontSize = `${currentSize * 1.5}px`;
 }
